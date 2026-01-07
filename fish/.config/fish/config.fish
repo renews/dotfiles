b744f6fa7b53672a9c6742d693ade8b7c85f1593
Rene Schneider <53726+renews@users.noreply.github.com>
2026-01-07 13:32:51 -0300

    fix fish ssh evals

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,22 @@
+if status is-login
+    and status is-interactive
+#only ask pw 1 time for ssh key git
+    keychain --eval --quiet ~/.ssh/id_ed25519 | source
+end
 if status is-interactive
-    mise activate fish | source
+  mise activate fish | source
+  atuin init fish | source
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  set -gx HOMEBREW_PREFIX (brew --prefix)
+
+  zoxide init fish | source
+  fzf --fish | source
+  starship init fish | source
 else
-    mise activate fish --shims | source
-    atuin init fish | source
+  mise activate fish --shims | source
 end
 
-#only ask pw 1 time for ssh key git
-eval (keychain --eval --quiet ~/.ssh/id_ed25519)
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-set -gx HOMEBREW_PREFIX (brew --prefix)
 
-zoxide init fish | source
-fzf --fish | source
-starship init fish | source
 # GLOBALS
 set -gx EDITOR nvim
 set -gx SHELL fish
@@ -22,4 +26,4 @@ set -Ux PAGER less
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 
-set -gx GPG_TTY $(tty)
+set -gx GPG_TTY (tty)
